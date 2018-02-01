a35a4f75580e9d67403229fa6aa3b6adbdd73985
eignnx <eignnx@gmail.com>
2018-01-31 19:53:03 -0500

    Get most functions mostly working

--- dynarray.c
+++ dynarray.c
@@ -29,6 +29,11 @@ void *_dynarray_create(size_t init_cap, size_t stride)
     return (void *) (arr + DYNARRAY_FIELDS);
 }
 
+void _dynarray_destroy(void *arr)
+{
+    free(arr - DYNARRAY_FIELDS * sizeof(size_t));
+}
+
 // Returns the dynarray's field which is specified by passing
 // one of CAPACITY, LENGTH, STRIDE.
 size_t _dynarray_field_get(void *arr, size_t field)
@@ -36,15 +41,37 @@ size_t _dynarray_field_get(void *arr, size_t field)
     return ((size_t *)(arr) - DYNARRAY_FIELDS)[field];
 }
 
-void _dynarray_push(void *arr, void *xptr)
+void _dynarray_field_set(void *arr, size_t field, size_t value)
 {
-    memcpy(arr + dynarray_length(arr) * dynarray_stride(arr), xptr, dynarray_stride(arr));
+    ((size_t *)(arr) - DYNARRAY_FIELDS)[field] = value;
 }
 
-// Removes the last element in the array, but copys it to `*dest` first.
+// Allocates a new dynarray with twice the size of the one passed in, and retaining
+// the values that the original stored.
+void *_dynarray_resize(void *arr)
+{
+    void *temp = _dynarray_create(2 * dynarray_capacity(arr), dynarray_stride(arr)); // Reallocate
+    memcpy(temp, arr, dynarray_length(arr) * dynarray_stride(arr)); // Copy erythin' over.
+    _dynarray_field_set(temp, LENGTH, dynarray_length(arr)); // Set `length` field.
+    _dynarray_destroy(arr); // Free previous array.
+    return temp;
+}
+
+void *_dynarray_push(void *arr, void *xptr)
+{
+    if (dynarray_length(arr) >= dynarray_capacity(arr))
+        arr = _dynarray_resize(arr);
+
+    memcpy(arr + dynarray_length(arr) * dynarray_stride(arr), xptr, dynarray_stride(arr));
+    _dynarray_field_set(arr, LENGTH, dynarray_length(arr) + 1);
+    return arr;
+}
+
+// Removes the last element in the array, but copies it to `*dest` first.
 void _dynarray_pop(void *arr, void *dest)
 {
-    //memcpy();
+    memcpy(dest, arr + (dynarray_length(arr) - 1) * dynarray_stride(arr), dynarray_stride(arr));
+    _dynarray_field_set(arr, LENGTH, dynarray_length(arr) - 1); // Decrement length.
 }
 
 
--- dynarray.h
+++ dynarray.h
@@ -19,14 +19,23 @@ enum {
 };
 
 void *_dynarray_create(size_t length, size_t stride);
+void _dynarray_destroy(void *arr);
+
 size_t _dynarray_field_get(void *arr, size_t field);
+void _dynarray_field_set(void *arr, size_t field, size_t value);
+
+void *_dynarray_resize(void *arr);
+
+void *_dynarray_push(void *arr, void *xptr);
+void _dynarray_pop(void *arr, void *dest);
 
 #define DYNARRAY_DEFAULT_CAP 1
 
 #define dynarray_create(type) _dynarray_create(DYNARRAY_DEFAULT_CAP, sizeof(type))
 #define dynarray_create_prealloc(type, capacity) _dynarray_create(capacity, sizeof(type))
 
-#define dynarray_append(arr, x) _dynarray_append(arr, x)
+#define dynarray_push(arr, x) _dynarray_push(arr, x)
+#define dynarray_pop(arr, xptr) _dynarray_pop(arr, xptr)
 
 #define dynarray_capacity(arr) _dynarray_field_get(arr, CAPACITY)
 #define dynarray_length(arr) _dynarray_field_get(arr, LENGTH)
