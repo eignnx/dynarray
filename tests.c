22c7dde89f5343649cae69b1497e9dc12efa5f45
eignnx <eignnx@gmail.com>
2018-01-31 19:52:05 -0500

    Add simple demonstration of create and push

--- tests.c
+++ tests.c
@@ -4,5 +4,14 @@
 
 int main()
 {
-    void *ints = dynarray_create(int);
+    int *ints = dynarray_create(int);
+    
+    for (int i = 0; i < 10; i++) {
+        int temp = 10 - i;
+        ints = dynarray_push(ints, &temp);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("element %d:\t%d\n", i, ints[i]);
+    }
 }
