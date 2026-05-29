e85e4c4b4b815bcef0d1dc5b01f3b10322f5cd7f
NanoDev <b.rego.oliveira@gmail.com>
2026-05-29 13:01:17 +0100

    adding documentation in dynarray.h

--- dynarray.h
+++ dynarray.h
@@ -4,6 +4,59 @@
 #include <stdlib.h>     // malloc
 #include <string.h>     // memcpy
 
+/**
+ * @mainpage Dynamic Array Library: dynarray
+ *
+ * @section overview Overview
+ * A generic dynamic array implementation in C.
+ * A dynarray of type T can be passed to any function that operates on vanilla T arrays.
+ * Inspiration taken from this article by solarianprogrammer.
+ *
+ * @section features Features
+ * - Automatic resizing
+ * - Generic storage using void pointers
+ * - Bounds-checked access (optional API)
+ *
+ * @section usage Basic Usage
+ * @code
+ * #include "dynarray.h"
+ *
+ * int *v = dynarray_create(int);
+ *
+ * for (int i = 0; i < 10; i++) {
+ *     dynarray_push(v, i);
+ *     dynarray_push_rval(v, 100 - i);
+ * }
+ *
+ * for (int i = 0; i < dynarray_length(v); i++) {
+ *     printf("v[%d] -> %d\n", i, v[i]);
+ * }
+ *
+ * dynarray_destroy(v);
+ * @endcode
+ * Output:
+ * @code
+ * v[0] -> 0
+ * v[1] -> 100
+ * v[2] -> 1
+ * v[3] -> 99
+ * ...
+ * v[18] -> 9
+ * v[19] -> 91
+ * @endcode
+ * @section Memory Layout
+ * The array is heap-allocated and is prefixed with a three-field header containing the buffer's capacity, length, and stride.
+ *
+ * - The stride is calculated at creation time as sizeof(T) where T is the datatype you intend to store.
+ * - The capacity field stores the buffer's size.
+ * - The length field keeps track of the number of elements stored in the buffer.
+ *
+ * Macros defined in dynarray.h allow the capacity, length, and stride attributes to be accessed.
+ *
+ * A dynarray is referred to only by a pointer to the first element in its buffer.
+ * This allows a dynarray to be passed to any function that operates on regular C-arrays.
+ */
+
 /* Structure of a dynarray:
  * size_t capacity
  * size_t length
@@ -32,7 +85,13 @@ void _dynarray_pop(void *arr, void *dest);
 #define DYNARRAY_DEFAULT_CAP 1
 #define DYNARRAY_RESIZE_FACTOR 2
 
+/**
+ * @brief 
+ *
+ * @param type
+ */
 #define dynarray_create(type) _dynarray_create(DYNARRAY_DEFAULT_CAP, sizeof(type))
+
 #define dynarray_create_prealloc(type, capacity) _dynarray_create(capacity, sizeof(type))
 #define dynarray_destroy(arr) _dynarray_destroy(arr)
 
